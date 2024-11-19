1ef34691e0285238a1a75a511ebbc611052376bc
v-shub <victoriya.shubina.mex@gmail.com>
2024-11-19 12:29:10 +0300

    third

--- src/WpfBasis/WpfBasis/Window3.xaml.cs
+++ src/WpfBasis/WpfBasis/Window3.xaml.cs
@@ -30,5 +30,27 @@ namespace WpfBasis
             pw.Show();
             this.Close();
         }
+
+        private void FindInterval_Click(object sender, RoutedEventArgs e)
+        {
+            string intervalString = "Промежуток: ";
+            if (!Int32.TryParse(NumBox.Text, out int interval))
+                MessageBox.Show("Введено не целое число");
+            else
+            {
+                if (interval < 0 | interval > 100)
+                    IntervalLabel.Content = "Число не попадает ни в один из промежутков";
+                else if (interval < 15)
+                    IntervalLabel.Content = intervalString + "0 - 14";
+                else if (interval < 36)
+                    IntervalLabel.Content = intervalString + "15 - 35";
+                else if (interval < 50)
+                    IntervalLabel.Content = intervalString + "36 - 50";
+                else if (interval == 50)
+                    IntervalLabel.Content = "Промежутки: 36 - 50 и 50 - 100";
+                else
+                    IntervalLabel.Content = intervalString + "50 - 100";
+            }
+        }
     }
 }
