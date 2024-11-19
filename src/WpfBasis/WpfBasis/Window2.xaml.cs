527974d59471fac64e508c31c55a8c96e21f9b7d
v-shub <victoriya.shubina.mex@gmail.com>
2024-11-19 12:04:03 +0300

    second

--- src/WpfBasis/WpfBasis/Window2.xaml.cs
+++ src/WpfBasis/WpfBasis/Window2.xaml.cs
@@ -30,5 +30,38 @@ namespace WpfBasis
             pw.Show();
             this.Close();
         }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string wrongInputMessage = "Вы ввели что-то не то";
+            bool isOperand1 = Int32.TryParse(Operand1Box.Text, out int operand1);
+            bool isOperand2 = Int32.TryParse(Operand2Box.Text, out int operand2);
+            if (!isOperand1 || !isOperand2)
+                MessageBox.Show(wrongInputMessage);
+            else
+            {
+                switch (SignBox.Text)
+                {
+                    case "+":
+                        ResultLabel.Content = operand1 + operand2;
+                        break;
+                    case "-":
+                        ResultLabel.Content = operand1 - operand2;
+                        break;
+                    case "*":
+                        ResultLabel.Content = operand1 * operand2;
+                        break;
+                    case "/":
+                        if(operand2==0)
+                            MessageBox.Show("Делить на ноль нельзя!");
+                        else
+                            ResultLabel.Content = operand1 / (double)operand2;
+                        break;
+                    default:
+                        MessageBox.Show(wrongInputMessage);
+                        break;
+                }
+            }
+        }
     }
 }
